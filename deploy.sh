aa571d18285dad664694c5e76fe7ef10575068c3
tal-nagra <tal.nagra@digital.cabinet-office.gov.uk>
2022-10-12 15:17:35 +0100

    fix local dev deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
-set -eu
+set -e
+
+stack_name="$1"
+
+if [ -z "$stack_name" ]
+then
+echo -e "Stack name expected as first argument, e.g. ./deploy.sh my-driving-permit-api"
+exit 1
+fi
+
 ./gradlew
+
 sam validate -t infrastructure/lambda/template.yaml --config-env dev
+
 sam build -t infrastructure/lambda/template.yaml --config-env dev
-sam deploy --config-file samconfig.toml --no-fail-on-empty-changeset -t infrastructure/lambda/template.yaml --config-env dev
+
+sam deploy --stack-name $stack_name \
+   --no-fail-on-empty-changeset \
+   --no-confirm-changeset \
+   --resolve-s3 \
+   --region eu-west-2 \
+   --capabilities CAPABILITY_IAM \
+   --parameter-overrides Environment=dev

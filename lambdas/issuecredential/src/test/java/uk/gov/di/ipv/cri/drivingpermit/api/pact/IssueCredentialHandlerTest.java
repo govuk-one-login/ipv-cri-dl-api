3443d564d7d356b6034741e27c941bb508e9988a
Seamus McShane <seamus.mcshane@digital.cabinet-office.gov.uk>
2025-12-08 15:12:31 +0000

    LIME-2021 Override org.apache.tika transitive dependency version, align pact SelectorBuilder with Fraud/Passport

--- lambdas/issuecredential/src/test/java/uk/gov/di/ipv/cri/drivingpermit/api/pact/IssueCredentialHandlerTest.java
+++ lambdas/issuecredential/src/test/java/uk/gov/di/ipv/cri/drivingpermit/api/pact/IssueCredentialHandlerTest.java
@@ -130,7 +130,7 @@ class IssueCredentialHandlerTest {
 
     @au.com.dius.pact.provider.junitsupport.loader.PactBrokerConsumerVersionSelectors
     public static SelectorBuilder consumerVersionSelectors() {
-        return new SelectorBuilder().branch("main", "IpvCoreBack").deployedOrReleased();
+        return new SelectorBuilder().mainBranch();
     }
 
     @BeforeEach

89ada0f09963c2a6e5ac514a59031b02982df9a8
Chris Bates <chris.bates@digital.cabinet-office.gov.uk>
2026-06-04 10:58:43 +0100

    LIME-2104 - Update Infra Test with QualityGateStackTest Tag
    The LambdaTemplateTest and CertificateTemplateTest files has been updated and the @QualityGateStackTest has been included along side the "InfraTest" Tag

--- acceptance-tests/src/test/java/gov/di_ipv_drivingpermit/infra/CertificateTemplateTest.java
+++ acceptance-tests/src/test/java/gov/di_ipv_drivingpermit/infra/CertificateTemplateTest.java
@@ -25,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @Tag("InfraTest")
+@Tag("QualityGateStackTest")
 @DisplayName("Certificate CloudFormation Template Assertions")
 class CertificateTemplateTest {
 
